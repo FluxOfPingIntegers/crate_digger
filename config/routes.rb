b14ab8ef12ea3cfb5fe8ff9b6073ae0dad0d1826
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-07-13 15:38:01 -0400

    routes mapped, sessions needs testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,23 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-  get 'comments/create'
-  get 'comments/index'
-  get 'comments/edit'
-  get 'comments/update'
-  get 'comments/destroy'
-  get 'stores/show'
-  get 'cities/index'
-  get 'cities/show'
-  get 'session/create'
-  get 'session/destroy'
-  resources :users
+  
+  root to: "static#home"
+  
+  get "login", to: "static#login", as: :login
+  
+  post "sessions", to: "session#create", as: :new_session # needs testing
+  
+  delete "sessions", to: "session#destroy", as: :logout # needs testing
+  
+  resources :users, only: [:new, :create, :show, :edit, :update, :destroy]
+  
+  resources :states, only: [:index]
+  
+  resources :states, only: [:show] do
+    resources :cities, only: [:index, :show]
+  end
+  
+  resources :stores, only: [:show] do
+    resources :comments, only: [:new, :create, :edit, :update, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
