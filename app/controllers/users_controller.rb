f4a67077da579ef4b238d0a5f757b72ce637612f
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-07-16 15:44:58 -0400

    users controller create and strong_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,18 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      flash[:errors] = @user.errors.full_messages
+      render :new
+    end
   end
 
   def show
@@ -16,5 +25,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :name, :password, :password_confirmation)
   end
 end
