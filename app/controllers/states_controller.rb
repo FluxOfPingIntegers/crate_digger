f5efbe954553d77d83ca407bdde2e13b943788ed
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-07-18 11:06:54 -0400

    moving state show to cities index

--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -8,6 +8,11 @@ byebug
   end
 
   def show
-    @state = params[:state_id]
+    state = params[:state_id]
+    if !!state
+      redirect_to state_cities_path(state)
+    else
+      redirect_to states_path
+    end
   end
 end
