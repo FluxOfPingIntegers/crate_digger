bc6ff41801982dd9ddcd7612cdffced12c210faf
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-07-18 14:03:41 -0400

    helper for states

--- app/helpers/states_helper.rb
+++ app/helpers/states_helper.rb
@@ -1,2 +1,13 @@
 module StatesHelper
+  def city_count_for(state)
+    count = state.cities.count
+    case count
+    when 1
+        "Only 1 city listed."
+    when 2..999
+        "#{count} cities listed."
+    else
+        "No listings for this state"
+    end
+  end
 end
