761137b4604a62a30d0906a21425897c36542b76
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-07-17 21:44:54 -0400

    sessions controller complete

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,5 +1,13 @@
 class SessionController < ApplicationController
   def create
+    user = User.find_by(username: params[:username])
+    if user.authenticate(params[:password]) && !!user
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash[:errors] = ["Incorrect Username and/or Password"]
+      redirect_to login_path
+    end
   end
 
   def destroy
