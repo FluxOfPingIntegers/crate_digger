5d3ef7a511323082bf5ec188feda9009ed09ce52
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-07-19 21:38:46 -0400

    fixed user show, working on omniauth

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,9 +1,14 @@
 class SessionController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
-    if user.authenticate(params[:password]) && !!user
-      session[:user_id] = user.id
-      redirect_to user_path(user)
+    if !!user
+      if user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to user_path(user)
+      else
+        flash[:errors] = ["Incorrect Username and/or Password"]
+        redirect_to login_path
+      end
     else
       flash[:errors] = ["Incorrect Username and/or Password"]
       redirect_to login_path
