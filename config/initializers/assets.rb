7c4ef89b774fd5ecaa957817624c9f040f7c13cb
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-07-23 09:37:42 -0400

    added css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+# Adding fonts folder within assets folder and enabling rails to find it
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
